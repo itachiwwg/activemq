ea17b1532fa09bc326448c030bbd12fb9be3283b
wwg-pc <361788095@qq.com>
2019-02-27 00:45:43 +0800

    new

---
+++ activemq-broker/src/main/java/org/apache/activemq/selector/SelectorParser.java
@@ -0,0 +1,14 @@
+package org.apache.activemq.selector;
+
+import org.apache.activemq.filter.BooleanExpression;
+
+public class SelectorParser {
+
+    public static void clearCache() {
+    }
+
+    public static BooleanExpression parse(String selector) {
+
+        return null;
+    }
+}
